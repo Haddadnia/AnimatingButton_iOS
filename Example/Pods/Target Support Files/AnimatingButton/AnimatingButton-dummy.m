2d638b5180668179229dabcc084c6c542052ae86
jhaddadnia <jhaddadnia@hotmail.com>
2016-10-16 18:16:23 -0400

    pod

---
+++ Example/Pods/Target Support Files/AnimatingButton/AnimatingButton-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AnimatingButton : NSObject
+@end
+@implementation PodsDummy_AnimatingButton
+@end
---
+++ Example/Pods/Target Support Files/Pods-AnimatingButton_Tests/Pods-AnimatingButton_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_AnimatingButton_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_AnimatingButton_TestsVersionString[];
+
